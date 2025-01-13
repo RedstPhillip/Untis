85a9ffc3fbf9fb150a42a1df47a6d0fee681f176
RedstPhillip <phillip.lutz@kufnet.at>
2025-01-13 19:21:23 +0100

    Changes by Raphael

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from rich import print
 from rich.prompt import Prompt, Confirm
+from rich.console import Console
+import designs
+import student
 
 import sqlite3
+
+import teacher
+
 con = sqlite3.connect('data.db')
 c = con.cursor()
 
 
 def login(table):
-    print("[bold green]You are logging in")
     while True:
         name = input("Enter your name: ").strip().title()
         password = input("Enter your password: ").strip()
@@ -42,24 +47,76 @@ def login(table):
             continue
 
     print(f"[green]Hello {name}! You successfully logged in as {name}\n")
-    return name
+
+    print(f"Table: {table}")
+    name = name.title()
+    print(f"Name: {name}")
+    c.execute(f"SELECT id FROM {table} WHERE name = ?", (name,))
+    user_id = c.fetchone()[0]
+    print(f"User_id: {user_id}")
+    return user_id
 
 
-def login_menu() -> str:
+def student_menu(id):
+    designs.student_menu()
+    option = input("Enter your option: ").strip().lower()
+
+    match option:
+        case "1" | "notifications":
+            pass
+        case "2" | "grades":
+            pass
+        case "3" | "absences":
+            pass
+        case "4" | "rate":
+            student.rate_teacher(id)
+        case "5" | "goal":
+            student.set_goal(id)
+        case "6" | "suggest":
+            pass
+        case "7" | "exit":
+            raise SystemExit("Bye!")
+
+
+def teacher_menu(id):
+    designs.teacher_menu()
+    option = input("Enter your option: ").strip().lower()
+
+    match option:
+        case "1" | "chat":
+            pass
+        case "2" | "test":
+            teacher.create_test(id)
+        case "3" | "announcement":
+            pass
+        case "4" | "grades":
+            pass
+        case "5" | "absence":
+            pass
+        case "6" | "suggest":
+            pass
+        case "7" | "exit":
+            raise SystemExit("Bye!")
+
+
+
+def login_menu():
     option = Prompt.ask("Log in as:", choices=["Administrator", "Teacher", "Student", "Parent"], default="Student")
     match option:
         case "Administrator":
             pass
         case "Teacher":
-            return login("teachers")
+            id = login("teachers")
+            main_menu = teacher_menu(id)
         case "Student":
-            return login("students")
+            student_id = login("students")
+            main_menu = student_menu(student_id)
         case "Parent":
-            return login("parents")
+            login("parents")
 
 
 def main():
-    username = login_menu()
+    login_menu()
 
 
 if __name__ == "__main__":
